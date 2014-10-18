40df4d536e0e7d6ae82f70d43ee4e70952711a3a
mccomput3rfr3ak <info.janva@gmail.com>
2014-10-18 15:40:51 +0200

    15:40 - regular commit

--- Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
@@ -1,25 +1,25 @@
 package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-//import com.comphenix.protocol.ProtocolLibrary;
-//import com.comphenix.protocol.ProtocolManager;
-
-//import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 
 public class DevathlonMccomput3rfr3ak extends JavaPlugin{
 
 	public static String prefix = ChatColor.GREEN + "[DevAhtlon-2014] " + ChatColor.RESET;
 	int countdown;
 	public boolean activeGame = false;
-//	private ProtocolManager protocolManager;
-//	
-//	public void onLoad() {
-//		protocolManager = ProtocolLibrary.getProtocolManager();
-//	}
+	private ProtocolManager protocolManager;
+	
+	public void onLoad() {
+		protocolManager = ProtocolLibrary.getProtocolManager();
+	}
 	
 	@Override
 	public void onDisable() {
@@ -43,13 +43,24 @@ public class DevathlonMccomput3rfr3ak extends JavaPlugin{
 				if(!activeGame) {
 					if(!Events.preparing) {
 						if (countdown == 0) {
-							
+							getServer().broadcastMessage(prefix + ChatColor.GOLD + "M\u00F6gen die Spiele beginnen!");
+							for (Player p : getServer().getOnlinePlayers()) {
+								p.playSound(p.getLocation(), Sound.NOTE_PLING, 100, 2);
+							}
+							activeGame = true;
+							this.cancel();
+						}
+						if (countdown == 60 || countdown == 30 || countdown == 15 || countdown == 5){
+							getServer().broadcastMessage(prefix + "Start in " + countdown + "!");
+							for (Player p : getServer().getOnlinePlayers()) {
+								((CraftPlayer) p).getHandler().playerConnection();
+							}
 						}
 					}
 				}
 				
 			}
-		};
+		}.runTaskTimerAsynchronously(this, 0L, 20L );
 	}
 	
 	public void timer() {
@@ -65,7 +76,7 @@ public class DevathlonMccomput3rfr3ak extends JavaPlugin{
 				} else this.cancel();
 				
 			}
-		}.runTaskTimerAsynchronously(this, 0L, 20L);
+		}.runTaskTimerAsynchronously(this, 0L, 200L);
 	}
 
 }
