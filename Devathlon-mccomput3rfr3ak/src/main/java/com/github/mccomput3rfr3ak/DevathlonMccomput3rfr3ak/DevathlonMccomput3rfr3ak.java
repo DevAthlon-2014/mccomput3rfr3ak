49166b6ad2e44a885489f95b5f53bb1639c558d9
mccomput3rfr3ak <info.janva@gmail.com>
2014-10-18 11:42:02 +0200

    11:42 - regular commit 4

--- Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
@@ -1,10 +1,13 @@
 package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathlonMccomput3rfr3ak extends JavaPlugin{
 
+	public static String prefix = ChatColor.RED + "[DevAhtlon-2014] " + ChatColor.RESET;
+	
 	@Override
 	public void onDisable() {
 		
--- Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/Events.java
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/Events.java
@@ -1,7 +1,16 @@
 package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener{
+
+	public String prefix = DevathlonMccomput3rfr3ak.prefix;
 	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		event.setJoinMessage(prefix + "Welcome, " + event.getPlayer().getName());
+		
+	}
 }
