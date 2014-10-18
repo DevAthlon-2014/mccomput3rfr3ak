9b9c2179f80885bce71dc53f5857757dad099cfa
mccomput3rfr3ak <info.janva@gmail.com>
2014-10-18 13:40:31 +0200

    13:40 - regular commit

--- Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/Events.java
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/Events.java
@@ -1,14 +1,26 @@
 package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Events implements Listener{
 
 	public String prefix = DevathlonMccomput3rfr3ak.prefix;
 	
+	/*!
+	 * Prepares the effectgame for the player
+	 */
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		
@@ -18,7 +30,26 @@ public class Events implements Listener{
 		p.sendMessage(prefix + "Welcome, " + p.getName());
 		p.getWorld().strikeLightningEffect(p.getLocation());
 		
+		ItemStack startGame = new ItemStack(Material.BLAZE_ROD);
+		ItemMeta startGameM = startGame.getItemMeta();
+		startGameM.setDisplayName(ChatColor.GOLD + "Starte das Effekt-Spiel");
 		
+		List<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.AQUA + "Klicke mit diesem Item");
+		lore.add(ChatColor.AQUA + "und lasse das Spiel beginnen");
 		
+		startGameM.setLore(lore);
+		startGame.setItemMeta(startGameM);
+		p.getInventory().setItem(4, startGame);
+		
+	}
+	
+	/*!
+	 * Prevent players who aren't in gamemode 1 from InventoryInteract
+	 */
+	@EventHandler
+	public void onPlayerInventory(InventoryClickEvent event) {
+		Player p = (Player) event.getWhoClicked();
+		if(p.getGameMode() != GameMode.CREATIVE) event.setCancelled(true);
 	}
 }
