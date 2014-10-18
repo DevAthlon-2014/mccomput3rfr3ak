71600a7127514d9ba9a2dac0e62241b7c68fd97c
mccomput3rfr3ak <info.janva@gmail.com>
2014-10-18 11:12:33 +0200

    11:12 - regular commit 3

--- Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/DevathlonMccomput3rfr3ak.java
@@ -1,5 +1,23 @@
 package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
 
-public class DevathlonMccomput3rfr3ak {
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DevathlonMccomput3rfr3ak extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+		
+		PluginDescriptionFile pdfFile = this.getDescription();
+        getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+	}
+	
+	
 
 }
---
+++ Devathlon-mccomput3rfr3ak/src/main/java/com/github/mccomput3rfr3ak/DevathlonMccomput3rfr3ak/Events.java
@@ -0,0 +1,7 @@
+package com.github.mccomput3rfr3ak.DevathlonMccomput3rfr3ak;
+
+import org.bukkit.event.Listener;
+
+public class Events implements Listener{
+	
+}
